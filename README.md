5594bfdd5ca159f86872abbdef7290cfe636f22b
Dolopex <dolopex2004@hotmail.com>
2023-10-16 13:23:27 -0500

    Parte 4

--- README.md
+++ README.md
@@ -21,3 +21,17 @@
 3.3 Definimos "numero"
 
 3.4 Se agrega una una posibilidad al codigo donde si se presiona la tecla "n" la terminal se borre y muestre un numero desde 0 a 50, borrando la terminal cada que se presione la tecla "n" y sumando "+1" al numero anterior.
+
+4.0 Se corrigió la posicion de los imports
+
+4.1 Se creó una variable donde se contiene el laberinto y esta pasa a ser una matriz y se define una funcion que lo muestra
+
+4.2 Se define la posicion inicial y final con (Px, Py)
+
+4.3 Se crea una funcion (mover_personaje) en la que se definen los movimientos modificando las coordenadas al presionar (Up,Down,Left,Right) respectivamente
+
+4.4 Se crea una funcion (jugar) que muestre el laberinto y use la funcion de mover personaje para empezar a jugar
+
+4.5 Al presionar "Up" se borra la terminal y me muestra el mapa del laberinto completamente funcional
+
+4.6 Al completar el laberinto se muestra un mensaje de felicitaciones (no he podido hacer que el mensaje se muestre, me pueden ayudar con esto, por favor)
--- main.py
+++ main.py
@@ -1,48 +1,124 @@
-#Importamos la libreria
-import readchar
 import os
+from readchar import readkey, key
 
-key = readchar.readkey()
-
-# Definimos la función para borrar la terminal
 def borrar_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-#Definimos Jugador
-Jugador = (input("Escribe Tu Nombre:"))
-print(Jugador)
-print(f"Bienvenido/a", Jugador) 
+def mapa(cadena_mapa):
+    filas = cadena_mapa.strip().split('\n')
+    laberinto = []
 
-#Definimos Teclas de movimiento
-print("Muevete usando (UP,DOWN,LEFT,RIGHT)")
+    for fila in filas:
+        caracteres_fila = list(fila)
+        laberinto.append(caracteres_fila)
 
-from readchar import readkey, key #No puedo subir este "import", si lo hago el codigo no funciona, como lo puedo solucionar?
+    return laberinto
+
+def mover_personaje(laberinto, direccion, px, py):
+    nueva_px, nueva_py = px, py
+
+    if direccion == key.DOWN:
+        nueva_px += 1
+    elif direccion == key.LEFT:
+        nueva_py -= 1
+    elif direccion == key.RIGHT:
+        nueva_py += 1
+    elif direccion == key.UP:
+        nueva_px -= 1
+
+    if (
+        0 <= nueva_px < len(laberinto) and
+        0 <= nueva_py < len(laberinto[0]) and
+        laberinto[nueva_px][nueva_py] != '#'
+    ):
+        laberinto[px][py] = '.'
+        px, py = nueva_px, nueva_py
+        laberinto[px][py] = 'P'
+
+    return px, py
+
+def jugar(laberinto, px, py, posicion_final):
+    while (px, py) != posicion_final:
+        mostrar_mapa(laberinto)
+        direccion = readkey()
+
+        if direccion in [key.DOWN, key.LEFT, key.RIGHT, key.UP]:
+            px, py = mover_personaje(laberinto, direccion, px, py)
+
+        if (px, py) == posicion_final:
+            break
+
+def mostrar_mapa(laberinto):
+    borrar_terminal()
+    for fila in laberinto:
+        print("".join(fila))
+
+cadena_mapa = """
+..###################
+....................#
+#.###.#.###.###.#####
+#.#.#.#.#.....#.#...#
+#.#.###.#.###.#####.#
+#.#.#...#.#...#.#...#
+###.#.###.#.###.###.#
+#.#.#.#...#.........#
+#.#.#.#.#.###.###.###
+#.....#.#.#.....#...#
+#.#.#.###.###.#####.#
+#.#.#...#...#.#.....#
+###.#.###.#######.###
+#...#...#...#.......#
+###########.###.#####
+#...#.......#.....#.#
+#.###.#######.#####.#
+#...#.......#...#.#.#
+#.#.#.#########.#.#.#
+#.#.....#...........#
+###################..
+"""
+
+matriz_laberinto = mapa(cadena_mapa)
+
+posicion_inicial = (0, 0)
+posicion_final = (21, 21)
+
+px, py = posicion_inicial
+matriz_laberinto[px][py] = 'P'
+
+Jugador = input("Escribe Tu Nombre: ")
+print(f"Bienvenido/a, {Jugador}")
+
+print("Probemos los controles: Muevete usando (UP, DOWN, LEFT, RIGHT). Y presiona n para sumar un número")
+print("Cuando estés listo, presiona UP para iniciar el laberinto.")
 
 numero = 0
 
 while True:
-  k = readkey()
- 
-  if k == key.DOWN:
-      print("Abajo")
-     
-  elif k == key.LEFT:
-      print("Izquierda")
-    
-  elif k == key.RIGHT:
-      print("Derecha")
-      
-  elif k == key.UP:
-    print("Arriba")
-    break
-  
-  elif k == 'n':
-    borrar_terminal()  # Borra la terminal
-    print(f"Número actual: {numero}")
-    numero += 1
-    if numero > 50:
-     numero = 0  # Reinicia el número a 0
-  else:
-       print(k)
+    k = readkey()
 
+    if k == key.DOWN:
+        print("Abajo")
 
+    elif k == key.LEFT:
+        print("Izquierda")
+
+    elif k == key.RIGHT:
+        print("Derecha")
+
+    elif k == key.UP:
+        print("Arriba")
+        mostrar_mapa(matriz_laberinto)
+        px, py = jugar(matriz_laberinto, px, py, posicion_final)
+        
+    if (px, py) == posicion_final:
+            print("Felicidades, Has completado el laberinto")
+            break  
+
+    elif k == 'n':
+        borrar_terminal()
+        print(f"Número actual: {numero}")
+        numero += 1
+        if numero > 50:
+            numero = 0
+    else:
+        print(k)
